b7ebfed38485cdb7074d90b981cfb7f27c5e236d
rohitgupta13130 <rohitgupta1310@gmail.com>
2024-09-14 19:04:38 +0530

    Skill Creatitng ClassLibrary file

---
+++ Controllers/SkillController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using IndiaTechingClassLibray.DAL;
+using IndiaTechingClassLibray.Request;
+using IndiaTechingClassLibray.Models;
+
+namespace IndiaTeachingWebAPI.Controllers
+{
+    public class SkillController : ApiController
+    {
+        // GET: api/Skill
+        public IEnumerable<Skill> Get()
+        {
+            List<Skill> skills = new SkillDAL().GetSkillList(new SkillRequest());
+            return skills;
+        }
+
+        // GET: api/Skill/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Skill
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Skill/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Skill/5
+        public void Delete(int id)
+        {
+        }
+    }
+}

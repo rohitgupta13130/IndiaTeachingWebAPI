5047e25c54b587781d1ce0c342665559c32229c6
Shadab Alam <shadabalamcse0@gmail.com>
2025-02-21 17:34:57 +0530

    notes controller

--- Controllers/NotesController.cs
+++ Controllers/NotesController.cs
@@ -2,6 +2,9 @@
 using India_Teaching.DAL;
 using India_Teaching.Models;
 using India_Teaching.Request;
+using IndiaTechingClassLibray.DAL;
+using IndiaTechingClassLibray.Request;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -47,8 +50,8 @@ namespace IndiaTeachingWebAPI.Controllers
         }
 
 
-        // POST: api/notes
-        [HttpPost]
+        //POST: api/notes
+       [HttpPost]
         public HttpResponseMessage SaveNotes([FromBody] Notes notes, HttpPostedFileBase file)
         {
             try
@@ -62,7 +65,55 @@ namespace IndiaTeachingWebAPI.Controllers
             }
         }
 
-        
+        //[HttpPost]
+        //public HttpResponseMessage SaveNotes()
+        //{
+        //    try
+        //    {
+        //        var httpRequest = HttpContext.Current.Request;
+        //        var file = httpRequest.Files["file"];
+        //        var notes = JsonConvert.DeserializeObject<Notes>(httpRequest.Form["notes"]);
+
+        //        HttpPostedFileBase fileBase = file != null ? new HttpPostedFileWrapper(file) : null;
+
+        //        int notesId = new NotesDAL().SaveNotes(notes, fileBase);
+        //        return Request.CreateResponse(HttpStatusCode.OK, notesId);
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex.Message);
+        //    }
+        //}
+
+
+        // DELETE: api/Notes/5
+        [HttpDelete]
+        public HttpResponseMessage Delete(int id)
+        {
+            try
+            {
+                if (id <= 0)
+                {
+                    return Request.CreateErrorResponse(HttpStatusCode.BadRequest, "Invalid ID.");
+                }
+
+                NotesRequest notesRequest = new NotesRequest { Id = id };
+                bool isDeleted = new NotesDAL().DeleteNotes(notesRequest);
+
+                if (isDeleted)
+                {
+                    return Request.CreateResponse(HttpStatusCode.OK, "Notes deleted successfully.");
+                }
+                else
+                {
+                    return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Notes not found or could not be deleted.");
+                }
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, ex.Message);
+            }
+        }
 
 
 
